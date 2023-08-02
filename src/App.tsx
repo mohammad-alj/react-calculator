84e5f9999c0b02c3b9120bb291cd33e3ef93eadf
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-02 16:09:51 +0300

    Create the style of the grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import Button from './components/Button';
+import ButtonGrid from './components/ButtonGrid';
 
 const App = () => {
 	return (
 		<div>
-			<Button onclick={() => console.log('1')} style='number'>
-				1
-			</Button>
-			<Button onclick={() => console.log('2')} style='number'>
-				2
-			</Button>
-			<Button onclick={() => console.log('3')} style='number'>
-				3
-			</Button>
-			<Button onclick={() => console.log('=')} style='operator'>
-				=
-			</Button>
-			<Button onclick={() => console.log(')')} style='symbol'>
-				)
-			</Button>
-			<Button onclick={() => console.log('DEL')} style='operation'>
-				DEL
-			</Button>
+			<ButtonGrid />
 		</div>
 	);
 };
--- src/components/ButtonGrid/ButtonGrid.tsx
+++ src/components/ButtonGrid/ButtonGrid.tsx
@@ -1,7 +1,36 @@
 import styles from './ButtonGrid.module.css';
+import Button from '../Button';
 
 const ButtonGrid = () => {
-	return <div>ButtonGrid</div>;
+	return (
+		<div className={styles.grid}>
+			{/* row 1 */}
+			<Button style='operation'>AC</Button>
+			<Button style='operation'>C</Button>
+			<Button style='operation'>DEL</Button>
+			<Button style='operator'>/</Button>
+			{/* row 2 */}
+			<Button style='number'>7</Button>
+			<Button style='number'>8</Button>
+			<Button style='number'>9</Button>
+			<Button style='operator'>*</Button>
+			{/* row 3 */}
+			<Button style='number'>4</Button>
+			<Button style='number'>5</Button>
+			<Button style='number'>6</Button>
+			<Button style='operator'>-</Button>
+			{/* row 4 */}
+			<Button style='number'>1</Button>
+			<Button style='number'>2</Button>
+			<Button style='number'>3</Button>
+			<Button style='operator'>+</Button>
+			{/* row 5 */}
+			<Button style='symbol'>(</Button>
+			<Button style='number'>0</Button>
+			<Button style='symbol'>)</Button>
+			<Button style='operator'>=</Button>
+		</div>
+	);
 };
 
 export default ButtonGrid;
