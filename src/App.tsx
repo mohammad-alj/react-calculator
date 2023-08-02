cabca36bcb3b7594eb9f3a120a224d091633a92b
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-02 08:41:01 +0300

    Change button width.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,23 @@ import Button from './components/Button';
 const App = () => {
 	return (
 		<div>
-			<Button onclick={() => console.log('clicked')} style='number'>
-				click me
+			<Button onclick={() => console.log('1')} style='number'>
+				1
+			</Button>
+			<Button onclick={() => console.log('2')} style='number'>
+				2
+			</Button>
+			<Button onclick={() => console.log('3')} style='number'>
+				3
+			</Button>
+			<Button onclick={() => console.log('=')} style='operator'>
+				=
+			</Button>
+			<Button onclick={() => console.log(')')} style='symbol'>
+				)
+			</Button>
+			<Button onclick={() => console.log('DEL')} style='operation'>
+				DEL
 			</Button>
 		</div>
 	);
