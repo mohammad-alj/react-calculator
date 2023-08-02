ada03bf0d0e075e3fe02fc7d66e20cf3f07cbe54
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-02 16:51:34 +0300

    Center the calculator.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import ButtonGrid from './components/ButtonGrid';
 import OutputWindow from './components/OutputWindow';
+import './App.css';
 
 const App = () => {
 	return (
-		<div>
-			{/* <ButtonGrid
+		<div className='calculator'>
+			<OutputWindow previous='2' operator='+' current='4' />
+			<ButtonGrid
 				onSelectSymbol={symbol => console.log(symbol)}
 				onAllClear={() => console.log('Cleared')}
 				onDelete={() => console.log('deleted')}
 				onEquals={() => console.log('equals')}
 				onSelectPoint={() => console.log('.')}
-			/> */}
-			<OutputWindow previous='2' operator='+' current='4' />
+			/>
 		</div>
 	);
 };
