2ac1ce4404a60181838d50bd2faaa40d77ff6adb
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-02 08:19:13 +0300

    Create base button style.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,11 @@
+import styles from './Button.module.css';
+
 interface Props {
 	children: string;
 }
 
 const Button = ({children}: Props) => {
-	return <button>{children}</button>;
+	return <button className={styles.btn}>{children}</button>;
 };
 
 export default Button;
