a9053b3f4324fb1eee003b13dbbb194351ced66e
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-02 08:37:42 +0300

    Create style feild so the user can choose a style.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Button from './components/Button';
 const App = () => {
 	return (
 		<div>
-			<Button onclick={() => console.log('clicked')}>click me</Button>
+			<Button onclick={() => console.log('clicked')} style='number'>
+				click me
+			</Button>
 		</div>
 	);
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,12 @@ import styles from './Button.module.css';
 
 interface Props {
 	children: string;
+	style: 'number' | 'operator' | 'symbol' | 'operation';
 	onclick?: () => void;
 }
 
-const Button = ({children}: Props) => {
-	return <button className={[styles.btn, styles.btnOperation].join(' ')}>{children}</button>;
+const Button = ({children, style}: Props) => {
+	return <button className={[styles.btn, styles['btn-' + style]].join(' ')}>{children}</button>;
 };
 
 export default Button;
